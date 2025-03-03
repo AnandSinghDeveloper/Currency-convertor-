169ad382d395b8b7b678bd7d24638e28f17dd5e1
Anand Thakur <anandsingh23112005@gmail.com>
2025-03-03 11:50:16 +0530

    ui create

--- src/Hooks/CurrencyInfo.js
+++ src/Hooks/CurrencyInfo.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CurrencyInfo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [data, setData] =  useState({})
+  
+   useEffect(( currency) => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+     .then(res => res.json())
+     .then(res => setData(res[currency]))
+     console.log(data);
+     
+   }, [currency])
+
+   console.log(data);
+   return data
+   
+  
 }
 
 export default CurrencyInfo
